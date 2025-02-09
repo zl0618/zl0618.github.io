de652f2598927da8cb374d440e889754cb3ac529
Zoe L. Lam <zoelam1215@outlook.com>
2025-02-10 00:18:54 +0800

    Create popup.js

---
+++ assets/js/popup.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var link = document.getElementById('medium-link');
+  if (link) {
+    link.addEventListener('click', function() {
+      window.open('https://medium.com/@zoelam1215_35712/how-did-i-start-my-first-machine-learning-project-b05c6c31bc57', '_blank', 'width=800,height=600');
+    });
+  }
+});
